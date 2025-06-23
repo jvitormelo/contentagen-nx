abdd73babbe5d207cd39906303f0254a71f01e34
Manoel <manoel.carvalhoneto@tutamail.com>
2025-06-23 01:10:10 -0300

    feat(landing-page): Add Cloudflare adapter and disable prerendering

--- apps/landing-page/astro.config.mjs
+++ apps/landing-page/astro.config.mjs
@@ -1,11 +1,10 @@
-
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
+import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
- 
+  adapter: cloudflare(),
   integrations: [react()],
 
   vite: {
