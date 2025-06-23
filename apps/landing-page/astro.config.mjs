7bd4b77f840c2f2ee0fb64c74582cba7258a8359
Manoel <manoel.carvalhoneto@tutamail.com>
2025-06-23 10:40:35 -0300

    feat(landing-page): Add VITE_SERVER_URL environment variable

--- apps/landing-page/astro.config.mjs
+++ apps/landing-page/astro.config.mjs
@@ -1,13 +1,17 @@
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   integrations: [react()],
   output: "server",
-
+  env: {
+    schema: {
+      VITE_SERVER_URL: envField.string(),
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
     resolve: {
