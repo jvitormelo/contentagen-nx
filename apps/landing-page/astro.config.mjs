6ad5731017f205fa7f4545b1eee75e14f2e903c5
Manoel <manoel.carvalhoneto@tutamail.com>
2025-06-23 10:31:20 -0300

    feat(landing-page): Use react-dom/server.edge for server output

--- apps/landing-page/astro.config.mjs
+++ apps/landing-page/astro.config.mjs
@@ -1,22 +1,20 @@
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   integrations: [react()],
   output: "server",
-  env: {
-    schema: {
-      VITE_SERVER_URL: envField.string({
-        context: "client",
-        access: "public",
-      }),
-    },
-  },
+
   vite: {
     plugins: [tailwindcss()],
-    resolve: {},
+    resolve: {
+      alias: {
+        "react-dom/server": "react-dom/server.edge",
+        "react-dom/server.browser": "react-dom/server.edge",
+      },
+    },
   },
 });
