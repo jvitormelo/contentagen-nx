1b833390b85809bd0c59a7e07c2701688bb84717
Manoel <manoel.carvalhoneto@tutamail.com>
2025-06-23 12:10:15 -0300

    feat(server): Integrate swagger and refactor Elysia app setup

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,37 @@
-import type { Env } from "bun";
-import type { Context } from "elysia";
-import { app } from "./app";
+import cors from "@elysiajs/cors";
+import swagger from "@elysiajs/swagger";
+import { Elysia } from "elysia";
+import { env } from "./config/env";
+import { authMiddleware, OpenAPI } from "./integrations/auth";
+import { agentRoutes } from "./routes/agent-routes";
+import { waitlistRoutes } from "./routes/waitlist-routes";
 
-export default {
-  async fetch(request: Request, env: Env, ctx: Context): Promise<Response> {
-    return await app.fetch(request);
-  },
-};
+const app = new Elysia()
+  .use(authMiddleware)
+  .use(
+    cors({
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+      origin: env.BETTER_AUTH_TRUSTED_ORIGINS.split(","),
+    }),
+  )
+  .use(
+    swagger({
+      documentation: {
+        components: await OpenAPI.components,
+        paths: await OpenAPI.getPaths(),
+      },
+    }),
+  )
+  .use(agentRoutes)
+  .use(waitlistRoutes)
+  .get("/works", () => {
+    return { message: "Eden WORKS!" };
+  })
+  .listen(9876);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
 export type App = typeof app;
--- apps/server/src/routes/waitlist-routes.ts
+++ apps/server/src/routes/waitlist-routes.ts
@@ -3,18 +3,23 @@ import { waitlist } from "@api/schemas/waitlist-schema";
 import { createInsertSchema } from "drizzle-typebox";
 import { Elysia, t } from "elysia";
 
-// 1. Generate the schema to infer types, as you require.
+const _createWaitlist = createInsertSchema(waitlist);
 
 export const waitlistRoutes = new Elysia({
   prefix: "/waitlist",
-}).post("/", async ({ body }) => {
-  // The `body` parameter is still correctly typed here!
-  const leadCreated = await db
-    .insert(waitlist)
-    .values({
-      email: body.email,
-      leadType: body.leadType,
-    })
-    .returning();
-  return { leadCreated };
-});
+}).post(
+  "/",
+  async ({ body }) => {
+    const leadCreated = await db
+      .insert(waitlist)
+      .values({
+        email: body.email,
+        leadType: body.leadType,
+      })
+      .returning();
+    return { leadCreated };
+  },
+  {
+    body: t.Omit(_createWaitlist, ["id", "createdAt"]),
+  },
+);
