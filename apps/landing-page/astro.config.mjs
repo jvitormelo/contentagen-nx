d055156bac072e60b9a5133fe12715720e667a38
Manoel <manoel.carvalhoneto@tutamail.com>
2025-06-26 10:19:19 -0300

    feat(landing-page): add sitemap integration

--- apps/landing-page/astro.config.mjs
+++ apps/landing-page/astro.config.mjs
@@ -1,5 +1,6 @@
 import node from "@astrojs/node";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
 export default defineConfig({
@@ -14,7 +15,7 @@ export default defineConfig({
       }),
     },
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   output: "server",
   server: {
     host: "0.0.0.0",
