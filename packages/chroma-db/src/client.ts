95b779f18089a434c9d0fc3078f06bc35bb03742
Manoel <manoel.carvalhoneto@tutamail.com>
2025-08-06 20:28:50 -0300

    refactor(chroma-db): simplify authentication headers in ChromaClient

--- packages/chroma-db/src/client.ts
+++ packages/chroma-db/src/client.ts
@@ -1,14 +1,12 @@
 import { serverEnv } from "@packages/environment/server";
 import { ChromaClient as InternalChromaCLient } from "chromadb";
+
 export const createChromaClient = (baseUrl: string): InternalChromaCLient => {
    return new InternalChromaCLient({
       path: baseUrl,
-      auth: {
-    provider: "token",
-    credentials: serverEnv.CHROMA_TOKEN,
-    tokenHeaderType: "AUTHORIZATION"
-  }
-      
+      headers: {
+         "Authorization": `Bearer ${serverEnv.CHROMA_TOKEN}`
+      }
    });
 };
 
