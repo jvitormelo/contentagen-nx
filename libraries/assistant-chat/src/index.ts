b8d29f87a47bc0b56fd07b05d12241d58fdfbab8
Manoel <manoel.carvalhoneto@tutamail.com>
2025-10-27 21:13:55 -0300

    chore(chore): remove libraries

--- libraries/assistant-chat/src/index.ts
+++
@@ -1,9 +0,0 @@
-export type { ContentaChatProps } from "./components/assistant-chat";
-export { ContentaChat } from "./components/assistant-chat";
-export type {
-	UseTypewriterOptions,
-	UseTypewriterReturn,
-} from "./hooks/useTypewriter";
-export { useTypewriter } from "./hooks/useTypewriter";
-export type { Locale, LocaleStrings } from "./lib/locale";
-export { DEFAULT_LOCALE, getLocaleStrings, LOCALE_STRINGS } from "./lib/locale";
